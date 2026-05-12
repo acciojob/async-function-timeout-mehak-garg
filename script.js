e74ddd36d1d15e6a4d60088ae98f52ff15dd7c17
mehak-garg <mehakgarg741@gmail.com>
2026-05-12 14:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let text=document.querySelector("#text");
+let delay=document.querySelector("#delay");
+let output=document.querySelector("#output");
+let btn=document.querySelector("#btn");
+
+function display(){
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{output.innerText=`${text.value}`;
+				   resolve("displayed");
+				   }
+			   ,Number(delay.value));
+	})
+} 
+async function display1()
+{
+await display();
+}
+btn.addEventListener("click",display1);
